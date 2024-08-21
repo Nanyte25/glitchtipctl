454186f45fa7618891942da65a96ef31eded4f14
mfreer <mfreer@redhat.com>
2024-08-21 10:53:54 +0100

    adding new update spinner model and get members features.

--- cmd/getUsers.go
+++ cmd/getUsers.go
@@ -1,17 +1,11 @@
 package cmd
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
@@ -19,7 +13,7 @@ import (
 var GetUsersCmd = &cobra.Command{
 	Use:   "getUsers",
 	Short: "Get a list of users from your organization",
-	Long:  `Get a list of users from your organization by making an HTTP GET request to the GlitchTip API and printing out the list of users.`,
+	Long:  `This command makes an HTTP GET request to the GlitchTip API and prints out the list of users from your organization.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		apiToken := os.Getenv("GLITCHTIP_API_TOKEN")
 		if apiToken == "" {
@@ -27,115 +21,41 @@ var GetUsersCmd = &cobra.Command{
 			return
 		}
 
-		// Start the spinner model
-		model := newSpinnerModel(apiToken)
+		// Use shared spinner model and fetch function
+		model := newModel(apiToken, fetchUsers(apiToken))
 		program := tea.NewProgram(model)
 
-		// Run the spinner and handle errors
+		// Start the program
 		if err := program.Start(); err != nil {
 			fmt.Printf("Error: %v\n", err)
 		}
 	},
 }
 
-// Spinner model to display the spinner while loading
-type spinnerModel struct {
-	spinner  spinner.Model
-	quitting bool
-	apiToken string
-	result   string
-	err      error
-}
+func fetchUsers(apiToken string) func() tea.Msg {
+	return func() tea.Msg {
+		url := "http://localhost:8000/api/0/users/"
+		headers := []string{"ID", "Name", "Email"}
 
-func newSpinnerModel(apiToken string) spinnerModel {
-	s := spinner.New()
-	s.Spinner = spinner.Dot
-	return spinnerModel{spinner: s, apiToken: apiToken}
-}
+		// Extract data function for formatting
+		extractData := func(body []byte) [][]string {
+			var users []map[string]interface{}
+			json.Unmarshal(body, &users)
 
-func (m spinnerModel) Init() tea.Cmd {
-	// Start the spinner and fetch users concurrently
-	return tea.Batch(m.spinner.Tick, m.fetchUsers)
-}
-
-func (m spinnerModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case spinner.TickMsg:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
-	case string:
-		m.quitting = true
-		m.result = msg
-		return m, tea.Quit
-	case error:
-		m.quitting = true
-		m.err = msg
-		return m, tea.Quit
-	}
-	return m, nil
-}
-
-func (m spinnerModel) View() string {
-	if m.quitting {
-		if m.err != nil {
-			return fmt.Sprintf("Error: %v\n", m.err)
+			data := [][]string{}
+			for _, user := range users {
+				id := fmt.Sprintf("%v", user["id"])
+				name := fmt.Sprintf("%v", user["name"])
+				email := fmt.Sprintf("%v", user["email"])
+				data = append(data, []string{id, name, email})
+			}
+			return data
 		}
-		return fmt.Sprintf("%s\n", m.result)
+
+		return fetchData(url, apiToken, headers, extractData)
 	}
-	return fmt.Sprintf("\n\n   %s Fetching users...\n\n", m.spinner.View())
-}
-
-func (m spinnerModel) fetchUsers() tea.Msg {
-	// Introduce a delay to simulate network latency (for testing)
-	time.Sleep(2 * time.Second) // Add artificial delay here for the spinner to display
-
-	// Make the API call to get users
-	url := "http://localhost:8000/api/0/users/"
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+m.apiToken)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body) // Replaces ioutil.ReadAll
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("HTTP request failed with status %d", resp.StatusCode)
-	}
-
-	var users []map[string]interface{}
-	if err := json.Unmarshal(body, &users); err != nil {
-		return err
-	}
-
-	// Create a table of users
-	var tableString bytes.Buffer
-	table := tablewriter.NewWriter(&tableString)
-	table.SetHeader([]string{"ID", "Name", "Email"})
-
-	for _, user := range users {
-		id := fmt.Sprintf("%v", user["id"])
-		name := fmt.Sprintf("%v", user["name"])
-		email := fmt.Sprintf("%v", user["email"])
-		table.Append([]string{id, name, email})
-	}
-	table.Render()
-
-	return tableString.String()
 }
 
 func init() {
-	// Add the getUsers command to the root
 	rootCmd.AddCommand(GetUsersCmd)
 }
